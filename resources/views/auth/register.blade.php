9d53d6501d4673789dfca4f93a787e8117379dd9
cumhurmesuttokalak <170260100@firat.edu.tr>
2021-12-24 09:07:10 +0300

    yapılıdı arayüz

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,48 +1,90 @@
-<?php
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
 
-namespace App\Actions\Fortify;
+        <x-jet-validation-errors class="mb-4" />
 
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-use Laravel\Fortify\Contracts\CreatesNewUsers;
-use Laravel\Jetstream\Jetstream;
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
 
-class CreateNewUser implements CreatesNewUsers
-{
-    use PasswordValidationRules;
+            <div>
+                <x-jet-label for="name" value="{{ __('Name') }}" />
+                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            </div>
 
-    /**
-     * Validate and create a newly registered user.
-     *
-     * @param  array  $input
-     * @return \App\Models\User
-     */
-    public function create(array $input)
-    {
-        Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'middle_name' => ['string', 'max:255'],
-            'last_name' => ['required', 'string', 'max:255'],
-            'tc_no' => ['required','string', 'max:11'],
-            'age' => ['required', 'numeric'],
-            'city' => ['required', 'string', 'max:255'],
-            'phone' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => $this->passwordRules(),
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
-        ])->validate();
+            <div class="mt-4">
+                <x-jet-label for="middle_name" value="Middle Name" />
+                <x-jet-input id="middle_name" class="block mt-1 w-full" type="text" name="middle_name" :value="old('middle_name')"/>
+            </div>
 
-        return User::create([
-            'first_name' => $input['name'],
-            'middle_name' => $input['middle_name'],
-            'last_name' => $input['last_name'],
-            'tc_no' => $input['tc_no'],
-            'age' => $input['age'],
-            'city' => $input['city'],
-            'phone_number' => $input['phone'],
-            'email' => $input['email'],
-            'password' => Hash::make($input['password']),
-        ]);
-    }
-}
+            <div class="mt-4">
+                <x-jet-label for="last_name" value="Last Name" />
+                <x-jet-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="age" value="Age" />
+                <x-jet-input id="age" class="block mt-1 w-full" type="number" name="age" :value="old('age')" required />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="city" value="City" />
+                <x-jet-input id="city" class="block mt-1 w-full" type="text" name="city" :value="old('city')" required />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="phone" value="Phone Number" />
+                <x-jet-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')" required />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="tc_no" value="TC No" />
+                <x-jet-input id="tc_no" class="block mt-1 w-full" type="text" name="tc_no" :value="old('tc_no')" required />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+            </div>
+
+            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+                <div class="mt-4">
+                    <x-jet-label for="terms">
+                        <div class="flex items-center">
+                            <x-jet-checkbox name="terms" id="terms"/>
+
+                            <div class="ml-2">
+                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
+                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
+                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
+                                ]) !!}
+                            </div>
+                        </div>
+                    </x-jet-label>
+                </div>
+            @endif
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+
+                <x-jet-button class="ml-4">
+                    {{ __('Register') }}
+                </x-jet-button>
+            </div>
+        </form>
+    </x-jet-authentication-card>
+</x-guest-layout>
